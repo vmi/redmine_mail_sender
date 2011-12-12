17c09e7fc3ac15b51f7fa229c619e6e02a67a31c
Motonori IWAMURO <vmi@nifty.com>
2011-12-12 20:09:38 +0900

    modify url to github.com.

--- init.rb
+++ init.rb
@@ -11,7 +11,7 @@ Redmine::Plugin.register :redmine_mail_sender do
   author 'IWAMURO Motonori'
   description 'Add "Sender:" field to mails sent by Redmine.'
   version '0.0.1'
-  url 'http://vmi.jp/software/ruby/redmine_mail_sender.html'
+  url 'https://github.com/vmi/redmine_mail_sender'
   author_url 'http://vmi.jp/'
 
   requires_redmine :version_or_higher => '1.2.0'
